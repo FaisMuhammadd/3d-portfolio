4aba103cecb616fbc7acdf4acf6c8ce7c5d40588
pandawaa <pallakangoding@gmail.com>
2024-01-04 13:07:49 +0800

    feat: Rotation, Bird, Plane, PopUps

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+import { Footer, Navbar } from "./components"
+import { About, Contact, Home, Projects } from "./pages"
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/*"
+            element={
+              <>
+                <Routes>
+                  <Route path="/about" element={<About />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
